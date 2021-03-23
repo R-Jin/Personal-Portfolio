a81797ae8ae9a449ba8dbfe21ffe12311334cbe9
R-Jin <ryanji007@gmail.com>
2021-03-23 10:37:47 +0100

    particles.js fix

--- app/js/particles/app.js
+++ app/js/particles/app.js
@@ -1,4 +1,3 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', './app/js/particles/particles.json', function() {
+particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
